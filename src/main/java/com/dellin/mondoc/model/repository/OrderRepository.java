faded8f61a38b8395d6d65ce77194e4c97d91005
Anton Filatov <antonfltvbox@gmail.com>
2023-03-02 15:55:04 +0400

    fix(entity): Fix LazyInitializationException

--- src/main/java/com/dellin/mondoc/model/repository/OrderRepository.java
+++ src/main/java/com/dellin/mondoc/model/repository/OrderRepository.java
@@ -4,16 +4,16 @@ import com.dellin.mondoc.model.entity.Company;
 import com.dellin.mondoc.model.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	
-	@Transactional
+	@EntityGraph(type = EntityGraph.EntityGraphType.FETCH, value = "order_entity-graph")
 	Optional<Order> findByDocId(String docId);
 	
 	Page<Order> findByCompanyIn(Collection<Company> companies, Pageable pageable);
